f96980fe58d101439748c744dfa88cfa029160af
Utsav Sharma <utsav19913@gmail.com>
2024-03-08 01:07:19 +0530

    added get card by id api

--- router/cardRouter.js
+++ router/cardRouter.js
@@ -101,6 +101,9 @@ cardRouter.put("/:id", async (req, res) => {
   }
 });
 
+
+
+
 //delete
 cardRouter.delete("/:id",async (req, res) => {
   const { id } = req.params;
@@ -201,5 +204,24 @@ cardRouter.get("/analytics",async (req, res) => {
     res.status(400).json({ error: "error occured" });
   }
 });
+
+//get card by id
+cardRouter.get("/:id",async (req, res) => {
+    const { id } = req.params;
+    try {
+      let response=await Card.findOne({ _id: id });
+      if (!response) {
+          return res.status(404).json({error:"card does not exist"});
+        }
+
+    // if(!response.isPublic){
+    //     return res.status(401).json({error:"unauthorized access"});
+    // }
+      return res.status(200).json(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ error: "error occured" });
+    }
+  });
 
 module.exports = cardRouter;
